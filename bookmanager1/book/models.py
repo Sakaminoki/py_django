5028e71e9c46d0503163970b0c9d92b51b642323
admin <320996299@qq.com>
2024-06-08 20:36:37 +0800

    bookmanager1基本增删改查

--- bookmanager1/book/models.py
+++ bookmanager1/book/models.py
@@ -6,6 +6,36 @@ from django.db import models
 2.定义属性
     属性名=models.类型(选项)
 """
-class Bookinfo(models.Model):
-    name=models.CharField(max_length=20)
-    
+
+
+class BookInfo(models.Model):
+    name=models.CharField(max_length=20,unique=True,verbose_name='名称')
+    pub_date=models.DateField(null=True,verbose_name='发布日期')
+    readcount=models.IntegerField(default=0,verbose_name='阅读量')
+    commentcount=models.IntegerField(default=0,verbose_name='评论量')
+    is_delete=models.BooleanField(default=False,verbose_name='逻辑删除')
+
+    class Meta:
+        db_table='bookinfo' # 修改表单的名字
+        verbose_name_plural='书籍管理' # admin站点要用
+
+    def __str__(self):
+        return self.name
+
+class PeopleInfo(models.Model):
+    GENDER_CHOICES = (
+        (0, 'male'),
+        (1, 'female')
+    )
+    name = models.CharField(max_length=20, verbose_name='名称')
+    gender = models.SmallIntegerField(choices=GENDER_CHOICES, default=0, verbose_name='性别')
+    description = models.CharField(max_length=200, null=True, verbose_name='描述信息')
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE, verbose_name='图书')  # 外键
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+    class Meta:
+        db_table = 'peopleinfo'
+        verbose_name_plural = '人物信息'
+
+    def __str__(self):
+        return self.name
--- bookmanager1/book/views.py
+++ bookmanager1/book/views.py
@@ -3,6 +3,119 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from book.models import BookInfo
 def index(request):
+    books=BookInfo.objects.all()
+    print(books)
 
-    return HttpResponse("django")
+    return HttpResponse(books)
+
+######增加数据######
+from book.models import BookInfo
+# 方式1
+book=BookInfo(
+    name='Django',
+    pub_date='2000-1-1',
+    readcount=10
+)
+# 要调用book.save()将数据保存到数据库中
+
+# 方式2，通过objects实现增删改查
+BookInfo.objects.create(
+    name='开发',
+    pub_date='2010-1-2',
+    readcount=100
+)
+
+######修改数据#####
+
+# 方式1
+book=BookInfo.objects.get(id=6)
+book.name='开'
+book.save()
+
+# 方式2
+BookInfo.objects.filter(id=6).update(name='爬虫开发')
+
+######删除数据######
+
+# 方式1
+
+book=BookInfo.objects.get(id=7)
+
+# 物理删除：删除数据   逻辑删除：修改标记为（如is_delete=False）
+
+book.delete()
+
+# 方式2
+BookInfo.objects.filter(id=6).delete()
+
+#######查询######
+
+# get查询单一结果，如果不存在会抛出 模型类.DoesNotExits异常
+try:
+    book = BookInfo.objects.get(id=1)
+except BookInfo.DoesNotExist:
+    print('查询结果不存在')
+
+# all查询多个结果
+BookInfo.objects.all()
+from  book.models import PeopleInfo
+PeopleInfo.objects.all()
+
+# count查询结果数量
+BookInfo.objects.all().count()
+BookInfo.objects.count() # 两条语句作用相同
+
+######过滤查询######
+# 实现sql中的where功能
+# filter过滤出多个结果
+# exclude删除掉符合条件剩下的结果
+# get得到单一结果
+
+# 模型类名.objects.felter(属性名__运算符=值)       获取n个结果
+# 模型类名.objects.exclude(属性名__运算符=值)      获取n个结果
+# 模型类名.objects.get(属性名__运算符=值)          获取1个结果，或者异常
+
+#查询编号为1的图书
+book=BookInfo.objects.get(id=1)      # 简写形式
+book=BookInfo.objects.get(id__exact=1) #完整形式
+
+BookInfo.objects.get(pk=1)          #pk：主键
+BookInfo.objects.filter(id=1)
+#查询书名包含'湖'的图书
+
+BookInfo.objects.filter(name__contains='湖')
+
+#查询书名以'部'结尾的图书
+
+BookInfo.objects.filter(name__endswith='部')
+
+#查询书名为空的图书
+
+BookInfo.objects.filter(name__isnull=True) # 不会报错
+BookInfo.objects.get(name__isnull=True) # 不存在会报错
+BookInfo.objects.get(name__isnull=False) # 查询结果有多个，也会报错
+
+#查询编号为1或3或5的图书
+
+BookInfo.objects.filter(id__in=[1,3,5])
+
+#查询编号大于3的图书
+# 大于 gt
+# 大于等于 gte
+# 小于lt
+# 小于等于lte
+BookInfo.objects.filter(id__gt=3)
+
+#查询编号不等于3的
+
+BookInfo.objects.exclude(id=3)
+
+#查询1980年发表的图书
+
+BookInfo.objects.filter(pub_date__year=1980)
+
+#查询1990年1月1日后发表的图书
+
+BookInfo.objects.filter(pub_date__gt='1990-1-1')
